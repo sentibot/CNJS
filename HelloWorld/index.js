5e7a059d8a62c5f3bd11d0347a3bb11f08dcfc1d
GLOBAL\I313418 <catalin.serbanescu@sap.com>
2017-07-24 11:25:09 +0300

    Return HW from server

--- HelloWorld/index.js
+++ HelloWorld/index.js
@@ -1,5 +1,21 @@
 "use strict";
 
+// require module
+var mHttp = require("http");
+
+// variables
+var port = 8080;
 var sMessage = "Hello World";
-sMessage += "!";
-console.log(sMessage);
+
+var oServer = mHttp.createServer(function(req, res){
+    console.log("========== REQUEST RECEIVED ==========");
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write(sMessage);
+
+    res.end();
+
+    console.log("========== RESPONSE SENT ==========");
+});
+oServer.listen(port);
+
