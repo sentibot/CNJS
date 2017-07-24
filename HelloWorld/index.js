42fa2fbce7deb2b2fbbd2e9185182212d507b80f
GLOBAL\I313418 <catalin.serbanescu@sap.com>
2017-07-24 11:40:38 +0300

    Return JSON Object

--- HelloWorld/index.js
+++ HelloWorld/index.js
@@ -7,11 +7,17 @@ var mHttp = require("http");
 var port = 8080;
 var sMessage = "Hello World";
 
+var oResponse = {
+    "returnCode":"1",
+    "returnMessage":"OK",
+    "returnData":null
+}
+
 var oServer = mHttp.createServer(function(req, res){
     console.log("========== REQUEST RECEIVED ==========");
 
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write(sMessage);
+    res.write(JSON.stringify(oResponse));
 
     res.end();
 
