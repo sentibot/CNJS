0b5e92d3a095945930a5479eb4e980dea68d1196
GLOBAL\I313418 <catalin.serbanescu@sap.com>
2017-07-26 10:21:51 +0300

    API Blueprint

--- MongoRestAPI/index.js
+++ MongoRestAPI/index.js
@@ -4,16 +4,29 @@ var express = require("express");
 var mongo = require("mongodb");
 var uuid = require("uuid/v4");
 
+var products = require("/modules/products.js");
+var orders = require("/modules/orders.js");
+
 var app = express();
 
-app.get("/", function(req, res){
-    res.send("Hello");
-});
+// products CRUD
+{
+    app.put("/products", function(req, res){
+        res.send();
+    });
 
-app.get("/", function(req, res){
-    res.send("Hello");
-});
+    app.post("/products", function(req, res){
+        res.send();
+    });
 
+    app.get("/products", function(req, res){
+        res.send();
+    });
+
+    app.delete("/product", function(req, res){
+        res.send();
+    });
+}
 
 app.listen(8080, function(){
     console.log("App listening on :8080");
