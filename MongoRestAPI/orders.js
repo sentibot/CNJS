7b0aab7ae395cb45bc5e9eddd9cc16fc9c6dca79
GLOBAL\I313418 <catalin.serbanescu@sap.com>
2017-07-26 10:17:46 +0300

    Corected dependecies location

---
+++ MongoRestAPI/orders.js
@@ -0,0 +1,19 @@
+exports.createOrder = module.exports.create = function createOrder(){
+    console.log("Product created");
+};
+
+exports.updateOrder = module.exports.create = function updateOrder(){
+    console.log("Product updated");
+};
+
+exports.listOrders = module.exports.create = function listOrders(){
+    console.log("Product created");
+};
+
+exports.getOrder = module.exports.create = function getOrder(){
+    console.log("Product created");
+};
+
+exports.deleteOrder = module.exports.create = function deleteOrder(){
+    console.log("Product created");
+};
---
+++ MongoRestAPI/products.js
@@ -0,0 +1,19 @@
+exports.createProduct = module.exports.create = function createProduct(){
+    console.log("Product created");
+};
+
+exports.updateProduct = module.exports.create = function updateProduct(){
+    console.log("Product updated");
+};
+
+exports.listProducts = module.exports.create = function listProducts(){
+    console.log("Product created");
+};
+
+exports.getProduct = module.exports.create = function getProduct(){
+    console.log("Product created");
+};
+
+exports.deleteProduct = module.exports.create = function deleteProduct(){
+    console.log("Product created");
+};
